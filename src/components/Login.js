1ed65d144cef67d8358a967f19032edb271a40e4
“Varshax” <varshiniak25@gmail.com>
2024-11-17 18:28:35 +0530

    misc

--- src/components/Login.js
+++ src/components/Login.js
@@ -122,6 +122,7 @@ const Login = () => {
                   required
                 />
               </div>
+
               <button
                 type="submit"
                 className="w-full bg-gradient-to-r from-indigo-500 to-purple-500 text-white py-3 px-4 rounded-lg font-medium
